c75f0cc015df65ac1229ef86b73598efb603e91f
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-01 17:22:47 +0200

    add: implement uppercase function to convert lowercase letters to uppercase

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    # Convert all lowercase letters in the string to uppercase.
+
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            print(chr(ord(c) - 32), end='')
+        else:
+            print(c, end='')
