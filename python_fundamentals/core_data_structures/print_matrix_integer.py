cacc9355c508258404a59bef029af87f31fec5ee
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-02 15:31:30 +0200

    feat: add print_matrix_integer function to print a matrix of integers

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers.
+
+    Args:
+        matrix (list of list of int): The matrix to print.
+    """
+    for row in matrix:
+        for idx, integer in enumerate(row):
+            if idx == len(row) - 1:
+                print(integer, end="")
+            else:
+                print(integer, end=" ")
+        print()
