c20f1ec21ee43741d42a6d3f4701c62a71a81bc0
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-07 13:52:39 +0200

    feat: update raise_exception function to raise NameError with a custom message

--- python_fundamentals/exception_handling/raise_exception.py
+++ python_fundamentals/exception_handling/raise_exception.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
-def raise_exception():
-    raise TypeError
+def raise_exception_msg(message=""):
+    """Raises a NameError with a custom message.
+
+    Args:
+        message (str): The message to include in the exception.
+    """
+    raise NameError(message)
