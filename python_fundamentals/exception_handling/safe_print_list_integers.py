2d106b4676775b980cab65e416ee22989fa00cf5
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-07 12:50:32 +0200

    feat: add safe_print_list_integers function to print the first x integers from a list

---
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Prints the first x elements of a list that are integers.
+
+    Args:
+        my_list (list): The list to print from.
+        x (int): The number of elements to print.
+
+    Returns:
+        int: The number of elements printed.
+    """
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except TypeError:
+            continue
+        except IndexError:
+            break
+    print()
+    return count
