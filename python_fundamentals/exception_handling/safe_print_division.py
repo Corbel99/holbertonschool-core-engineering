c2304821e7fd7f4755769e9a321ce373d3e810f3
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-07 13:34:16 +0200

    feat: add safe_print_division function to safely divide two integers and print the result

---
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Divides 2 integers and prints the result.
+
+    Args:
+        a: First integer.
+        b: Second integer.
+
+    Returns:
+        The result of the division, or None if division fails.
+    """
+    res = None
+    try:
+        res = a / b
+    except (ZeroDivisionError, TypeError):
+        pass
+    finally:
+        print("Inside result: {}".format(res))
+    return res
