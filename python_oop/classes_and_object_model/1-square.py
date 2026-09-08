029d8583d0187f800cc9250ce284e33a17f23614
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-08 11:53:51 +0200

    feat: add Square class implementation with initialization

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a square with a given size."""
+        self.__size = size
+    
