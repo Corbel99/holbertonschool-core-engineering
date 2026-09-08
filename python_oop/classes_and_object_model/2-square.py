5af6004345a81b4462017a7c6e341baf1ef99086
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-08 12:18:22 +0200

    fix: remove unnecessary spaces in Square class constructor

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Represents a square."""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Initializes a square with a given size."""
 
         if not isinstance(size, int):
