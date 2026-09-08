117e2bb88a80f870eb384a72974d7dfaedbe26ff
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-08 12:15:28 +0200

    feat: implement Square class with size validation

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a square with a given size."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
