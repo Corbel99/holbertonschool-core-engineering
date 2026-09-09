1b4c41bf3505b69e4ca599645b7e138bf0b33075
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-09 15:57:06 +0200

    feat: implement Square class inheriting from Rectangle with area calculation

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
+"""Defines the Square class that inherits from Rectangle."""
+
+Rectangle = __import__('2-rectangle').Rectangle
+
 
 class Square(Rectangle):
+    def __init__(self, size):
+        """Initialize a Square instance with size."""
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
