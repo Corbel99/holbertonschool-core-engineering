430e36229c31ef20fc4e6f6f8a4964770d613857
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-09 16:01:09 +0200

    feat: implement Square class with area calculation and string representation

--- python_oop/inheritance/2-square.py
+++ python_oop/inheritance/2-square.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
+"""Defines the Square class that inherits from Rectangle."""
 
-#!/usr/bin/env python3
+Rectangle = __import__('2-rectangle').Rectangle
+
 
 class Square(Rectangle):
+    """Represents a square."""
+    def __init__(self, size):
+        """Initialize a Square instance with size."""
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return a string representation of the square."""
+        return f"[Square] {self.__size}/{self.__size}"
