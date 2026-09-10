b276f38a910d69bb790417483d15b045d70c08be
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-10 11:53:07 +0200

    feat: implement Animal, Dog, and Cat classes with sound methods

--- python_oop/abc/animals.py
+++ python_oop/abc/animals.py
@@ -1,1 +1,42 @@
 #!/usr/bin/env python3
+
+"""This module defines an abstract base class for animals."""
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    Abstract base class for animals.
+    """
+
+    @abstractmethod
+    def sound(self):
+        """
+        Abstract method to be implemented by subclasses to make a sound.
+        """
+        pass
+
+
+class Dog(Animal):
+    """
+    Dog class that inherits from Animal.
+    """
+
+    def sound(self):
+        """
+        Implementation of the sound method for Dog.
+        """
+        return "Bark"
+
+
+class Cat(Animal):
+    """
+    Cat class that inherits from Animal.
+    """
+
+    def sound(self):
+        """
+        Implementation of the sound method for Cat
+        """
+        return "Meow"
