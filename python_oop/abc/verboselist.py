19ffebe41ee676c536d93f0a845be30e4460228f
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-10 17:21:12 +0200

    feat: enhance VerboseList class with detailed notifications for list modifications

--- python_oop/abc/verboselist.py
+++ python_oop/abc/verboselist.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
-#print hello world 
 
+class VerboseList(list):
+    """A list that prints notifications when modified."""
 
-def hello():
+    def append(self, item):
+        """Add an item and print a notification."""
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, x):
+        """Extend the list and print the number of items added."""
+        super().extend(x)
+        print(f"Extended the list with [{len(x)}] items.")
+
+    def remove(self, item):
+        """Remove an item and print a notification."""
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        """Remove and return an item with a notification."""
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
