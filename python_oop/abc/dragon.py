52191e88501f5e976d6952aa7a741e316b6c4681
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-10 16:46:53 +0200

    feat: implement Dragon class with swim and fly mixins

--- python_oop/abc/dragon.py
+++ python_oop/abc/dragon.py
@@ -1,1 +1,48 @@
 #!/usr/bin/env python3
+"""
+This module defines mixins for swimming and flying behaviors,
+and a Dragon class that combines them.
+"""
+
+
+class SwimMixin:
+    """
+    Mixin that provides swimming behavior.
+    """
+
+    def swim(self):
+        """
+        Make the creature swim.
+        """
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """
+    Mixin that provides flying behavior.
+    """
+
+    def fly(self):
+        """
+        Make the creature fly.
+        """
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """
+    Represents a dragon that can swim, fly, and roar.
+    """
+
+    def roar(self):
+        """
+        Make the dragon roar.
+        """
+        print("The dragon roars!")
+
+
+dragon = Dragon()
+
+dragon.swim()
+dragon.fly()
+dragon.roar()
