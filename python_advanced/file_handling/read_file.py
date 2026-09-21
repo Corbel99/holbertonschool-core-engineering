55d97823ed3d1b74db98a1bd9c95e1f048cd5736
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-21 10:46:29 +0200

    feat: add a blank line for improved readability in read_file.py

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module for reading and printing the contents of a text file."""
 
+
 def read_file(filename=""):
     """Read a text file and print its content to stdout."""
     with open(filename)as f:
