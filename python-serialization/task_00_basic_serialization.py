4b0c177f0198dab24209250e56018fe87a6294f7
Corbel99 <thomas.corbel35000@gmail.com>
2026-09-22 11:25:11 +0200

    fix: add shebang line to task_00_basic_serialization.py for script execution

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,11 +1,12 @@
-import json
+#!/usr/bin/env python3
 
+
+import json
 
 def serialize_and_save_to_file(data, filename):
     """Serialize a Python dictionary to a JSON file."""
     with open(filename, mode="w") as file:
         json.dump(data, file)
-
 
 def load_and_deserialize(filename):
     """Load a JSON file and deserialize it into a Python dictionary."""
